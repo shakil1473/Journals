fee98b412f8be61f58c68e2276a17c46a704018f
shakil mahmud <34098468+shakil1473@users.noreply.github.com>
2025-01-21 16:45:10 +0100

    Deleted
    Will commit from local as fresh

--- src/main/java/com/learning/journalApp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.learning.journalApp.repository;
-
-import com.learning.journalApp.entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-    User findByUsername(String username);
-}
